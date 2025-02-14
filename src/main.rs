89d9531bfba0d69b30e51cd1bedaa2e10708ff84
Serveny <dylan_lewis93@web.de>
2025-02-14 13:54:40 +0100

    Changed log level to INFO

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ const MODEL: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/model.
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug"))
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
         .filter(Some("tract"), LevelFilter::Warn)
         .init();
 
